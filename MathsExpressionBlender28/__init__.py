70c241eb7bd64e6f27d4f27ab71f8ad6e39755fc
Rich Sedman <rich@sedman.me.uk>
2019-03-04 12:56:37 +0000

    Implemented Edit mode within the group via a custom node This provides working Maths Expression functionality for Blender 2.8. This now needs to be backported to a version that works with Blender 2.79 (essentially just need to implement the operator 'draw' functions and change to use class variables instead of attributes.

--- MathsExpressionBlender28/__init__.py
+++ MathsExpressionBlender28/__init__.py
@@ -1,6 +1,6 @@
 # Author: Rich Sedman
 # Description: Dynamic Maths Expresion node Blender Add-on
-# Version: (0.90)
+# Version: (0.99)
 # Date: May 2018
 ################################################### History ######################################################
 # 0.4  01/06/2018 : Fix problems in parse_expression (parse_expression v0.3)
@@ -14,15 +14,16 @@
 # 0.61 05/02/2019 : Bring up to date for Blender 2.8 API changes
 # 0.90 01/03/2019 : Implement Edit and tidy up placement of created node.
 # 0.91 01/03/2019 : Remove unnecessary import of ShaderNodeCategory (no longer used)
+# 0.99 04/03/2019 : Implement Edit mode from custom node within group
 ##################################################################################################################
 
 bl_info = {  
  "name": "Dynamic Maths Expression",  
  "author": "Rich Sedman",  
- "version": (0, 90),  
+ "version": (0, 99),  
  "blender": (2, 80, 0),  
- "location": "Node Editor > Add > Custom_Nodes",  
- "description": "Adds a custom node that allows you to create a node tree from an arbitrary expression.",  
+ "location": "Node Editor > Add",  
+ "description": "Provide an option to create a new group from a typed maths expression.",  
  "warning": "",  
  "wiki_url": "https://github.com/baldingwizard/Blender-Addons/wiki",  
  "tracker_url": "https://github.com/baldingwizard/Blender-Addons/issues",  
@@ -30,18 +31,20 @@ bl_info = {
 
 import bpy
 
-from .dynamic_maths_expression_node import DynamicMathsExpression_Operator, DynamicMathsExpressionEdit_Operator
+from .dynamic_maths_expression_node import DynamicMathsExpression_Operator, DynamicMathsExpressionEdit_Operator, DynamicMathsExpressionEditWithin_Operator, DynamicMathsExpressionNode
 
 from nodeitems_utils import NodeItem, register_node_categories, unregister_node_categories
 
 def menu_draw(self, context):
-    self.layout.operator("node.node_dynamic_maths_expression", text='Maths Expression(New)')
-    self.layout.operator("node.node_dynamic_maths_expression_edit", text='Maths Expression(Edit)')
+    self.layout.operator("node.node_dynamic_maths_expression", text='Maths Expression')
+    
+    # Edit is not required since we can now edit within the group itself via the custom node
+    #self.layout.operator("node.node_dynamic_maths_expression_edit", text='Maths Expression(Edit)')
 
 bpy.types.NODE_MT_add.append(menu_draw)
 #TODO : Need to add it to Add/Group rather than Add
 
-classes = ( DynamicMathsExpression_Operator, DynamicMathsExpressionEdit_Operator)
+classes = ( DynamicMathsExpression_Operator, DynamicMathsExpressionEdit_Operator, DynamicMathsExpressionEditWithin_Operator, DynamicMathsExpressionNode)
 
 def register():
     from bpy.utils import register_class
