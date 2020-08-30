b5ed7e1764bbf9ca613a8040a9eab7cc304f4e7b
Rich Sedman <rich@sedman.me.uk>
2020-08-30 06:45:37 +0100

    New Fluid2EXR to replace Smoke2EXR
    The old Smoke2EXR used the Point Cache file to extract smoke information into a set of EXR images. This is slow and relies on files being in Point Cache format. The new add-on uses the data available in the Fluid modifier directly - which makes it compatible with the Mantaflow fluid in Blender 2.82+

---
+++ Fluid2EXR/__init__.py
@@ -0,0 +1,67 @@
+# Author: Rich Sedman
+# Description: Smoke to EXR - convert the pointcloud from a smoke domain into an EXR image
+# Date: August 2020
+################################################### History ######################################################
+# 0.10 14/06/2019 : Initial creation as an add-on based on previously developed scripts
+# 0.11 26/06/2019 : 'pack' the image and also add a fake user so that image is retained even if unused elsewhere
+#                   Also, set EXR codec to 'zip' to reduce packed size of image.
+# 0.12 26/08/2019 : Improve efficiency of 'pack', add better diagnostics as to progress, implement 'multiRow' mode
+# 0.13 26/08/2019 : Fix bug in running on 2.79 (failed to pack image)
+# 0.14 28/08/2019 : Implement hires for density, fuel/react/heat, RGB
+# 0.20 28/08/2020 : Use Smoke2EXR as a basis for same functionality to extract direct from Fluid modifier grids
+##################################################################################################################
+
+#TODO: Possibly bake Ambient Occlusion into the smoke color (optional setting)
+
+bl_info = {  
+ "name": "Fluid2EXR",
+ "author": "Rich Sedman",  
+ "version": (0, 20),  
+ "blender": (2, 80, 0),  
+ "location": "(operators)",  
+ "description": "Provide a means of converting a fluid simulation into an image that can be used to access the volumetric data independently of the simulation",  
+ "warning": "",  
+ "wiki_url": "",  
+ "tracker_url": "",  
+ "category": "Operator"}    #TODO: Don't know if this is correct!! 
+
+import bpy
+
+from .fluid2exr import Fluid2EXR_Operator
+
+#def menu_draw(self, context):
+#    #Force 'invoke' when calling operator
+#    self.layout.operator_context = "INVOKE_DEFAULT"
+#    
+#    self.layout.operator("node.node_dynamic_maths_expression", text='Maths Expression')
+#    
+#    # Edit is not required since we can now edit within the group itself via the custom node
+#    #self.layout.operator("node.node_dynamic_maths_expression_edit", text='Maths Expression(Edit)')
+#
+#bpy.types.NODE_MT_add.append(menu_draw)
+##TODO : Need to add it to Add/Group rather than Add
+
+classes = ( Fluid2EXR_Operator, )
+
+def add_to_panel(self, context):
+    layout = self.layout
+    layout.operator("operator.fluid2exr", text='Convert Fluid to EXR')
+
+#bpy.types.PHYSICS_PT_smoke_cache.append(add_to_panel)
+
+def register():
+    from bpy.utils import register_class
+    for cls in classes:
+        register_class(cls)
+
+def unregister():
+    from bpy.utils import unregister_class
+    for cls in reversed(classes):
+        unregister_class(cls)
+
+if __name__ == "__main__":
+    try:
+        unregister()
+    except:
+        pass
+    register()
---
+++ Fluid2EXR/fluid2exr.py
@@ -0,0 +1,339 @@
+# Script to convert a fluid simulation into an OpenEXR image
+
+# RAS 26/06/2019 : Added 'pack' and 'use_fake_user' for newly generated images
+# RAS 26/08/2019 : Improve efficiency of 'pack', add better diagnostics as to progress, implement 'multiRow' mode
+#                  - all changes developed as part of the MRI Raw to EXR script.
+# RAS 28/08/2019 : Implement hires capture
+
+#TODO: multiple frames to one exr. Also, image compression... - actually, now 'multirow', can't do multiple frames... perhaps that could be a sub-mode for single-row mode only.
+
+# Inspiration for the re-write taken from https://blender.stackexchange.com/a/182868/29586 by Leander
+
+import bpy
+import os
+import sys
+import math
+
+def convert_volume_to_exr(object, oPattern, oframeno, multiRow=False, hiresMultiplier=1):
+
+    #Set the frame
+    bpy.context.scene.frame_set(oframeno)
+    
+    #Get populated domain settings
+    domain_settings = object.evaluated_get(bpy.context.evaluated_depsgraph_get()).modifiers["Fluid"].domain_settings
+        
+    #if ...not smoke: 
+    #
+    #    print("Only 'smoke' type is currently processed - found "+str(flavor))
+    #else:
+    #    smokeversion = ....
+    #    print("Smoke version = "+str(smokeversion))
+    #    
+    #    (fluid_fields,active_fields, res_x, res_y, res_z, dx) = ......
+    #    print("Got fields %i\t%i\t%i\t%i\t%i\t%f" % (fluid_fields,active_fields, res_x, res_y, res_z, dx))
+    res_x = domain_settings.domain_resolution[0]
+    res_y = domain_settings.domain_resolution[1]
+    res_z = domain_settings.domain_resolution[2]
+    
+    if domain_settings.use_noise:
+        res_x = res_x * (domain_settings.noise_scale)
+        res_y = res_y * (domain_settings.noise_scale)
+        res_z = res_z * (domain_settings.noise_scale)
+    
+    SM_ACTIVE_HEAT		= 1
+    SM_ACTIVE_FIRE		= 2
+    SM_ACTIVE_COLORS	= 4
+    SM_ACTIVE_COLOR_SET	= 8
+
+    #TODO : ....'amplify' setting determines how many divisions the hires volume has. 1 -> 2x2x2, 2 -> 3x3x3, etc. - so multiply by amplify+1 in each dimension. Need to change the 'size' and also the 'dimension' by this factor.
+    #hiresmult = hiresMultiplier       # 'amplify'+1
+    hiresmult=1
+
+    size = res_x * res_y * res_z * 4
+    hiressize = size * hiresmult*hiresmult*hiresmult
+
+    #shadow = ....
+    density = domain_settings.density_grid
+
+    heat = None
+    heatold = None
+    #if (...doing heat) > 0:
+    #    heat = ....
+    #    heatold = ....
+        
+    flame = None
+    fuel = None
+    react = None
+    #if (....doing burny...) > 0:
+    #    flame = ....
+    #    fuel = ....
+    #    react = ....
+
+    rgb_r = None
+    rgb_g = None
+    rgb_b = None
+    #if (...doing colours...) > 0:
+    #    rgb_r = ....
+    #    rgb_g = ....
+    #    rgb_b = ....
+        
+    #vx = ....
+    #vy = ....
+    #vz = ....
+
+    #obstacles = ....
+
+    #extrafields = f.read(45*4)
+    #rec = struct.unpack("fffffffffffiiifffffffffffffffffffiiiiiiiiifff",extrafields)
+    #print("Extra Fields = "+str(rec))
+    
+    #hires_density = ....
+    #hires_flame = ....
+    #hires_fuel = ....
+    #hires_react = ....
+
+    #hires_rgb_r = None
+    #hires_rgb_g = None
+    #hires_rgb_b = None
+    #if (....doing colours(hires)...) > 0:
+    #    hires_rgb_r = ....
+    #    hires_rgb_g = ....
+    #    hires_rgb_b = ....
+
+    #tcu = ....
+    #tcv = ....
+    #tcw = ....
+
+    #TODO: Get HIRES working correctly
+    #print("WARNING: HIRES not working for flame, fuel, react")
+    hires_rgb_b = None
+    hires_flame = None
+    hires_density = None
+
+    if rgb_b != None:
+        build_exr_from_buffers(gen_filename("smoke",oPattern, oframeno), (res_x, res_y, res_z), rgb_r, rgb_g, rgb_b, None, multiRow=multiRow)
+    else:
+        build_exr_from_buffers(gen_filename("smoke",oPattern, oframeno), (res_x, res_y, res_z), density, density, density, None, multiRow=multiRow)
+
+    if hires_rgb_b != None:
+        build_exr_from_buffers(gen_filename("hires_smoke",oPattern, oframeno), (res_x*hiresmult, res_y*hiresmult, res_z*hiresmult), hires_rgb_r, hires_rgb_g, hires_rgb_b, None, multiRow=multiRow)          #TODO: This also seems to be 'wrong' if multi-colored!!!!
+    else:
+        if hires_density != None:
+            build_exr_from_buffers(gen_filename("hires_smoke",oPattern, oframeno), (res_x*hiresmult, res_y*hiresmult, res_z*hiresmult), hires_density, hires_density, hires_density, None, multiRow=multiRow)
+
+    #build_exr_from_buffers(gen_filename("velocity",oPattern, oframeno), (res_x, res_y, res_z), vx, vy, vz, None, multiRow=multiRow)
+
+    if flame != None:
+        build_exr_from_buffers(gen_filename("flame_heat_fuel",oPattern, oframeno), (res_x, res_y, res_z), flame, heat, fuel, None, multiRow=multiRow)
+            
+    if hires_flame != None:
+        #build_exr_from_buffers(gen_filename("hires_flame_react_fuel",oPattern, oframeno), (res_x*hiresmult, res_y*hiresmult, res_z*hiresmult), hires_flame, hires_react, hires_fuel, None, multiRow=multiRow)
+        #build_exr_from_buffers(gen_filename("hires_flame_react_fuel",oPattern, oframeno), (res_x, res_y, res_z), hires_flame, hires_react, hires_fuel, None, multiRow=multiRow)     #TODO: Determine why these buffers aren't hires and what they are... something to do with hires turbulence?!!!
+        build_exr_from_buffers(gen_filename("hires_flame_react_fuel",oPattern, oframeno), (res_x*hiresmult, res_y*hiresmult, res_z*hiresmult), hires_flame, hires_react, hires_fuel, None, multiRow=multiRow)     #TODO: Determine why these buffers aren't hires and what they are... something to do with hires turbulence?!!!
+
+
+    #f.close()
+
+# Generate filename by combining name, pattern, frameno
+def gen_filename(name, pattern, frameno):
+    return pattern % (name, frameno)
+
+
+def build_exr_from_buffers(filename, dimensions, bufferR, bufferG, bufferB, bufferA, multiRow=False):
+
+    if multiRow:
+        numColumns = math.ceil(math.sqrt(dimensions[2]))
+        numRows = math.ceil(dimensions[2] / numColumns)
+    else:
+        numColumns = dimensions[2]
+        numRows = 1
+
+    filename = str(dimensions[2])+"_"+str(numColumns)+"x"+str(numRows)+"_"+filename
+
+    print("Building image %s" % filename)
+
+    # Size the image to allow space for Z images of size X by Y
+    width = (dimensions[0]+1)*numColumns
+    if numRows >1:
+        height = (dimensions[1]+1)*numRows
+    else:
+        height = dimensions[1]
+
+    # Create the image
+    image = bpy.data.images.new(filename, width=width, height=height,float_buffer=True)
+
+    # Create an empty array of pixel data (each will hold R, G, B, A values as floats)
+    print("Clearing image...")
+    pixels = [None] * width * height
+    for x in range(0,width):
+        for y in range(0,height):
+            pixels[y*width+x] = [0.0,0.0,0.0,0.0]
+            
+    print("File '"+filename+"', Dimensions = ("+str(dimensions[0])+","+str(dimensions[1])+","+str(dimensions[2])+")")
+    
+    for z in range(0,dimensions[2]):
+        print("Processing layer "+str(z))
+        
+        #Calculate the location of this 'tile'
+        tileNoX = z % numColumns
+        tileNoY = int((z - tileNoX) / numColumns)
+        tileOffset = tileNoX*(dimensions[0]+1)+tileNoY*width*(dimensions[1]+1)
+
+        for x in range(0,dimensions[0]):
+        
+            for y in range(0,dimensions[1]):
+            
+                p = x+y*dimensions[0]+z*dimensions[0]*dimensions[1]
+
+                # If R, G, or B are 'none' then 0.0 is assumed
+                valR = 0
+                valG = 0
+                valB = 0
+                if bufferR != None:
+                    #valR = struct.unpack('f',bufferR[p*4:p*4+4])[0]
+                    valR = bufferR[p]
+                    
+                if bufferG != None:
+                    #valG = struct.unpack('f',bufferG[p*4:p*4+4])[0]
+                    valG = bufferG[p]
+
+                if bufferB != None:
+                    #valB = struct.unpack('f',bufferB[p*4:p*4+4])[0]
+                    valB = bufferB[p]
+
+                # bufferA can be None to indicate not used (in which case 1.0 is assumed)
+                if bufferA != None:
+                    #valA = struct.unpack('f',bufferA[p*4:p*4+4])[0]
+                    valA = bufferA[p]
+                else:
+                    valA = 1.0
+                
+               # pixels[(y*width)+x+z*(dimensions[0]+1)] = [valR,valG,valB,valA]
+                pixels[tileOffset + x + y*width] = [valR,valG,valB,valA]
+
+    # 'flatten' the array - so [R1,G1,B1,A1], [R2,G2,B2,A2], [R3,G3,B3,A3],.... becomes R1,B1,G1,A1,R2,G2,B2,A2,R3,G3,B3,A3,....    
+    # Store the pixels in the image
+    print("Image build complete, storing pixels...")
+    image.pixels = [chan for px in pixels for chan in px]
+    print("Updating image...")
+    image.update()
+
+    print("Saving image...")
+    # Save image to temporary file and pack it (from answer by @sambler)
+    scn = bpy.data.scenes.new('img_settings')
+    scn.render.image_settings.file_format = 'OPEN_EXR'
+    scn.render.image_settings.exr_codec = 'ZIP'
+    scn.render.image_settings.color_mode = 'RGBA'
+    scn.render.image_settings.color_depth = '32'
+    img_path = bpy.path.abspath('//')
+    img_file = image.name+'.exr'
+    image.save_render(img_path+img_file, scene=scn)
+    try:
+        # Remove the scene (pre-2.78)
+        bpy.data.scenes.remove(scn)
+    except:
+        # Remove the scene, (Blender 2.78+)
+        bpy.data.scenes.remove(scn, do_unlink=True)
+        
+    #Pack the images (needs to be saved and re-opened)
+    if bpy.app.version_string.startswith('2.79'):
+        bpy.data.images.remove(image)
+        bpy.ops.image.open(filepath=img_path+img_file)
+        image = bpy.data.images[img_file]
+        
+    image.pack()
+    if bpy.app.version_string.startswith('2.79'):
+        os.remove(img_path+img_file)
+    
+    image.use_fake_user = True
+    print("Complete.")
+
+class Fluid2EXR_Operator(bpy.types.Operator):
+    """Convert a fluid domain into an EXR image file"""
+    
+    bl_idname = "operator.smoke2exr"
+    bl_label = "Smoke2EXR - Convert smoke domain into an EXR image"
+    bl_space_type = 'VIEW_3D'
+    label = 'Smoke2EXR'
+    #bl_options = {}
+
+    frameNo: bpy.props.IntProperty(description='Enter the frame to convert', name='Frame')
+    smokeCacheName: bpy.props.StringProperty(description="The selection object's smoke cache name", name="Cache Name")
+    multiRow: bpy.props.BoolProperty(description='Create multi-row image instead of a single row of tiles', name="Multi-Row")
+       
+
+    def __init__(self):
+        super().__init__()
+        self.multiRow = True
+        
+
+    @classmethod
+    def poll(cls, context):
+        #space = context.space_data
+        #return space.type == 'VIEW_3D'
+        return True
+
+    def invoke(self, context, event):
+        wm = context.window_manager
+        
+        self.frameNo = context.scene.frame_current
+        
+        #viewWidth = bpy.context.area.width * 0.8
+        #
+        object_active = context.active_object
+        objects_selected = context.selected_objects
+        
+        if len(objects_selected) < 1:
+            self.report({'ERROR'}, 'Must select the smoke domain object')
+            return {"CANCELLED"}
+        
+        if len(objects_selected) > 1:
+            self.report({'ERROR'}, 'Must select only the single smoke domain object')
+            return {"CANCELLED"}
+            
+        selected_object = objects_selected[0]
+        
+        if not 'Fluid' in selected_object.modifiers.keys():
+            self.report({'ERROR'}, "Object must include a Fluid modifier named 'Fluid'")
+            return {"CANCELLED"}
+        
+        #TODO: Use the modifier type rather than name and also check that it's set to a *smoke domain*
+        
+        smokeCache = selected_object.modifiers['Fluid'].domain_settings.point_cache
+        
+        self.smokeCacheName = smokeCache.name
+        print("Smoke cache name = "+str(self.smokeCacheName))
+        
+        self.smokeCache = smokeCache
+        #self.domainSettings = smokeCache = selected_object.modifiers['Smoke'].domain_settings
+        self.domainSettings = selected_object.modifiers['Fluid'].domain_settings
+        self.domainObject = selected_object
+        
+        return wm.invoke_props_dialog(self)
+
+    def execute(self, context):
+  
+        smokeCacheName = str(self.smokeCacheName)
+        
+        if len(smokeCacheName) == 0:
+            self.report({'ERROR'}, "Smoke cache must be baked to a *named* cache.")
+            return {"CANCELLED"}
+            #TODO: Pickup the "random" generated name if we don't find an explicit name
+            
+        cachefile = bpy.path.abspath("//blendcache_%s/%s_%06i_00.bphys" % (bpy.path.basename(bpy.context.blend_data.filepath[:-6]),smokeCacheName,self.frameNo))
+        
+        #hiresMultiplier = self.domainSettings.amplify + 1
+        hiresMultiplier = 1
+        
+        convert_volume_to_exr(self.domainObject, "%s_%06i", self.frameNo, self.multiRow, hiresMultiplier=hiresMultiplier)
+        return {'FINISHED'}
+    
+
+#Run it....
+#for frame in range(10,11):
+#    cachefile = bpy.path.abspath("//blendcache_%s/smoke_%06i_00.bphys" % (bpy.path.basename(bpy.context.blend_data.filepath[:-6]),frame))
+#    convert_pointcache_volume_to_exr(cachefile, "output/%s_%06i", frame)
+
+#Possibly build up the filename using the name of the pointcache from :
+# bpy.data.objects["Smoke Domain"].modifiers["Smoke"].domain_settings.point_cache.name
+#...also, need to ensure the cache is set to a compatible 'mode' so that we can uncompress and interpret it.
