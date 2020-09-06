1211ffbd38be85bb843769bb249a376c9e554e1b
Rich Sedman <rich@sedman.me.uk>
2020-09-06 07:09:27 +0100

    Fluid2EXR working but inefficient for large dimensions. Investigating using Numpy to improve efficiency.

--- Fluid2EXR/fluid2exr.py
+++ Fluid2EXR/fluid2exr.py
@@ -8,13 +8,17 @@
 #TODO: multiple frames to one exr. Also, image compression... - actually, now 'multirow', can't do multiple frames... perhaps that could be a sub-mode for single-row mode only.
 
 # Inspiration for the re-write taken from https://blender.stackexchange.com/a/182868/29586 by Leander
+#
+# 1.00 30/08/2020 : Use '...._grid' data direct from the modifier rather than read from Pointcache files
 
 import bpy
 import os
 import sys
 import math
+import numpy as np
 
-def convert_volume_to_exr(object, oPattern, oframeno, multiRow=False, hiresMultiplier=1):
+#def convert_volume_to_exr(object, oPattern, oframeno, multiRow=False, hiresMultiplier=1):
+def convert_volume_to_exr(object, oPattern, oframeno, multiRow=False):
 
     #Set the frame
     bpy.context.scene.frame_set(oframeno)
@@ -34,23 +38,20 @@ def convert_volume_to_exr(object, oPattern, oframeno, multiRow=False, hiresMulti
     res_x = domain_settings.domain_resolution[0]
     res_y = domain_settings.domain_resolution[1]
     res_z = domain_settings.domain_resolution[2]
+    lowresgridsize = res_x * res_y * res_z
     
     if domain_settings.use_noise:
-        res_x = res_x * (domain_settings.noise_scale)
-        res_y = res_y * (domain_settings.noise_scale)
-        res_z = res_z * (domain_settings.noise_scale)
+        hires_x = res_x * (domain_settings.noise_scale)
+        hires_y = res_y * (domain_settings.noise_scale)
+        hires_z = res_z * (domain_settings.noise_scale)
+        hiresgridsize = hires_x * hires_y * hires_z
+    else:
+        hiresgridsize = lowresgridsize
     
     SM_ACTIVE_HEAT		= 1
     SM_ACTIVE_FIRE		= 2
     SM_ACTIVE_COLORS	= 4
     SM_ACTIVE_COLOR_SET	= 8
-
-    #TODO : ....'amplify' setting determines how many divisions the hires volume has. 1 -> 2x2x2, 2 -> 3x3x3, etc. - so multiply by amplify+1 in each dimension. Need to change the 'size' and also the 'dimension' by this factor.
-    #hiresmult = hiresMultiplier       # 'amplify'+1
-    hiresmult=1
-
-    size = res_x * res_y * res_z * 4
-    hiressize = size * hiresmult*hiresmult*hiresmult
 
     #shadow = ....
     density = domain_settings.density_grid
@@ -68,19 +69,22 @@ def convert_volume_to_exr(object, oPattern, oframeno, multiRow=False, hiresMulti
     #    flame = ....
     #    fuel = ....
     #    react = ....
-
-    rgb_r = None
-    rgb_g = None
-    rgb_b = None
-    #if (...doing colours...) > 0:
+    flame = domain_settings.flame_grid
+    heat = domain_settings.heat_grid
+    #...what about 'temperature_grid'?
+    velocity = domain_settings.velocity_grid
+    
+    #Now use numpy to split the x,y,z array into 3 separate arrays
+    velocityxyz = np.array(velocity)  #The basic numpy array
+    
+    #rgb_r = None
+    #rgb_g = None
+    #rgb_b = None
+    colorrgb = np.array(domain_settings.color_grid)
     #    rgb_r = ....
     #    rgb_g = ....
     #    rgb_b = ....
         
-    #vx = ....
-    #vy = ....
-    #vz = ....
-
     #obstacles = ....
 
     #extrafields = f.read(45*4)
@@ -104,32 +108,79 @@ def convert_volume_to_exr(object, oPattern, oframeno, multiRow=False, hiresMulti
     #tcv = ....
     #tcw = ....
 
+    if len(density) == lowresgridsize:
+        build_exr_from_buffers(gen_filename("smoke",oPattern, oframeno), (res_x, res_y, res_z), density, density, density, None, multiRow=multiRow)
+    elif len(density) == hiresgridsize:
+        build_exr_from_buffers(gen_filename("hires_smoke",oPattern, oframeno), (hires_x, hires_y, hires_z), density, density, density, None, multiRow=multiRow)
+    else:
+        print("density_grid unexpected size (%i)" % len(density))
+
+    if len(flame) == lowresgridsize:
+        build_exr_from_buffers(gen_filename("flame",oPattern, oframeno), (res_x, res_y, res_z), flame, flame, flame, None, multiRow=multiRow)
+    elif len(flame) == hiresgridsize:
+        build_exr_from_buffers(gen_filename("hires_flame",oPattern, oframeno), (hires_x, hires_y, hires_z), flame, flame, flame, None, multiRow=multiRow)
+    else:
+        print("flame_grid unexpected size (%i)" % len(flame))
+
+    if len(heat) == lowresgridsize:
+        build_exr_from_buffers(gen_filename("heat",oPattern, oframeno), (res_x, res_y, res_z), heat, heat, heat, None, multiRow=multiRow)
+    elif len(heat) == hiresgridsize:
+        build_exr_from_buffers(gen_filename("hires_heat",oPattern, oframeno), (hires_x, hires_y, hires_z), heat, heat, heat, None, multiRow=multiRow)
+    else:
+        print("heat_grid unexpected size (%i)" % len(heat))
+
+    if velocityxyz.any() and len(velocityxyz > 0):
+        velocityx = velocityxyz[0::3].tolist()   #Every 3rd element, starting at 0
+        velocityy = velocityxyz[1::3].tolist()   #Every 3rd element, starting at 1
+        velocityz = velocityxyz[2::3].tolist()   #Every 3rd element, starting at 2
+
+        if len(velocityxyz) == lowresgridsize*3:
+            build_exr_from_buffers(gen_filename("velocity",oPattern, oframeno), (res_x, res_y, res_z), velocityx, velocityy, velocityz, None, multiRow=multiRow)
+        elif len(velocityxyz) == hiresgridsize*3:
+            build_exr_from_buffers(gen_filename("hires_velocity",oPattern, oframeno), (hires_x, hires_y, hires_z), velocityx, velocityy, velocityz, None, multiRow=multiRow)
+        else:
+            print("velocity_grid unexpected size (%i)" % len(velocityxyz))
+
+    if colorrgb.any() and len(colorrgb) > 0:
+        colorr = colorrgb[0::4].tolist()   #Every 4th element, starting at 0
+        colorg = colorrgb[1::4].tolist()   #Every 4th element, starting at 1
+        colorb = colorrgb[2::4].tolist()   #Every 4th element, starting at 2
+        colora = colorrgb[3::4].tolist()   #Every 4th element, starting at 3
+
+        if len(colorrgb) == lowresgridsize*4:
+            build_exr_from_buffers(gen_filename("color",oPattern, oframeno), (res_x, res_y, res_z), colorr, colorg, colorb, colora, multiRow=multiRow)
+        elif len(colorrgb) == hiresgridsize*4:
+            build_exr_from_buffers(gen_filename("hires_color",oPattern, oframeno), (hires_x, hires_y, hires_z), colorr, colorg, colorb, colora, multiRow=multiRow)
+        else:
+            print("color_grid unexpected size (%i)" % len(colorrgb))
+
     #TODO: Get HIRES working correctly
     #print("WARNING: HIRES not working for flame, fuel, react")
-    hires_rgb_b = None
-    hires_flame = None
-    hires_density = None
+    #hires_rgb_b = None
+    #hires_flame = None
+    #hires_density = None
 
-    if rgb_b != None:
-        build_exr_from_buffers(gen_filename("smoke",oPattern, oframeno), (res_x, res_y, res_z), rgb_r, rgb_g, rgb_b, None, multiRow=multiRow)
-    else:
-        build_exr_from_buffers(gen_filename("smoke",oPattern, oframeno), (res_x, res_y, res_z), density, density, density, None, multiRow=multiRow)
+    #if rgb_b != None:
+    #    build_exr_from_buffers(gen_filename("smoke",oPattern, oframeno), (res_x, res_y, res_z), rgb_r, rgb_g, rgb_b, None, multiRow=multiRow)
+    #else:
+    #    build_exr_from_buffers(gen_filename("smoke",oPattern, oframeno), (res_x, res_y, res_z), density, density, density, None, multiRow=multiRow)
 
-    if hires_rgb_b != None:
-        build_exr_from_buffers(gen_filename("hires_smoke",oPattern, oframeno), (res_x*hiresmult, res_y*hiresmult, res_z*hiresmult), hires_rgb_r, hires_rgb_g, hires_rgb_b, None, multiRow=multiRow)          #TODO: This also seems to be 'wrong' if multi-colored!!!!
-    else:
-        if hires_density != None:
-            build_exr_from_buffers(gen_filename("hires_smoke",oPattern, oframeno), (res_x*hiresmult, res_y*hiresmult, res_z*hiresmult), hires_density, hires_density, hires_density, None, multiRow=multiRow)
+    #if hires_rgb_b != None:
+    #    build_exr_from_buffers(gen_filename("hires_smoke",oPattern, oframeno), (res_x*hiresmult, res_y*hiresmult, res_z*hiresmult), hires_rgb_r, hires_rgb_g, hires_rgb_b, None, multiRow=multiRow)          #TODO: This also seems to be 'wrong' if multi-colored!!!!
+    #else:
+    #    if hires_density != None:
+    #        build_exr_from_buffers(gen_filename("hires_smoke",oPattern, oframeno), (res_x*hiresmult, res_y*hiresmult, res_z*hiresmult), hires_density, hires_density, hires_density, None, multiRow=multiRow)
 
-    #build_exr_from_buffers(gen_filename("velocity",oPattern, oframeno), (res_x, res_y, res_z), vx, vy, vz, None, multiRow=multiRow)
+    #if velocity_grid != None:
+    #    build_exr_from_buffers(gen_filename("velocity",oPattern, oframeno), (res_x, res_y, res_z), vx, vy, vz, None, multiRow=multiRow)
 
-    if flame != None:
-        build_exr_from_buffers(gen_filename("flame_heat_fuel",oPattern, oframeno), (res_x, res_y, res_z), flame, heat, fuel, None, multiRow=multiRow)
+    #if flame != None:
+    #    build_exr_from_buffers(gen_filename("flame_heat_fuel",oPattern, oframeno), (res_x, res_y, res_z), flame, heat, fuel, None, multiRow=multiRow)
             
-    if hires_flame != None:
-        #build_exr_from_buffers(gen_filename("hires_flame_react_fuel",oPattern, oframeno), (res_x*hiresmult, res_y*hiresmult, res_z*hiresmult), hires_flame, hires_react, hires_fuel, None, multiRow=multiRow)
+    #if hires_flame != None:
+    #    #build_exr_from_buffers(gen_filename("hires_flame_react_fuel",oPattern, oframeno), (res_x*hiresmult, res_y*hiresmult, res_z*hiresmult), hires_flame, hires_react, hires_fuel, None, multiRow=multiRow)
         #build_exr_from_buffers(gen_filename("hires_flame_react_fuel",oPattern, oframeno), (res_x, res_y, res_z), hires_flame, hires_react, hires_fuel, None, multiRow=multiRow)     #TODO: Determine why these buffers aren't hires and what they are... something to do with hires turbulence?!!!
-        build_exr_from_buffers(gen_filename("hires_flame_react_fuel",oPattern, oframeno), (res_x*hiresmult, res_y*hiresmult, res_z*hiresmult), hires_flame, hires_react, hires_fuel, None, multiRow=multiRow)     #TODO: Determine why these buffers aren't hires and what they are... something to do with hires turbulence?!!!
+    #    build_exr_from_buffers(gen_filename("hires_flame_react_fuel",oPattern, oframeno), (res_x*hiresmult, res_y*hiresmult, res_z*hiresmult), hires_flame, hires_react, hires_fuel, None, multiRow=multiRow)     #TODO: Determine why these buffers aren't hires and what they are... something to do with hires turbulence?!!!
 
 
     #f.close()
@@ -140,6 +191,15 @@ def gen_filename(name, pattern, frameno):
 
 
 def build_exr_from_buffers(filename, dimensions, bufferR, bufferG, bufferB, bufferA, multiRow=False):
+
+    #...want to re-write this to use numpy functions.... could work something like this...
+    #1) split the array into 3-dimensional representation of pixels (should be one-liner, I think)
+    #2) add elements such that it will form a complete 'grid' of images (eg, 10x10)
+    #3) convert to a 4-d array with gridx, gridy, x, y*dimensions
+    #4) take each 'gridy' set and split the lines of each sub-array
+    #5) interleave those lines
+    #6) flatten it
+    #7) combine them all
 
     if multiRow:
         numColumns = math.ceil(math.sqrt(dimensions[2]))
@@ -170,7 +230,10 @@ def build_exr_from_buffers(filename, dimensions, bufferR, bufferG, bufferB, buff
             pixels[y*width+x] = [0.0,0.0,0.0,0.0]
             
     print("File '"+filename+"', Dimensions = ("+str(dimensions[0])+","+str(dimensions[1])+","+str(dimensions[2])+")")
-    
+ 
+    yrange = range(0,dimensions[1])
+    xrange = range(0,dimensions[0])
+ 
     for z in range(0,dimensions[2]):
         print("Processing layer "+str(z))
         
@@ -179,11 +242,16 @@ def build_exr_from_buffers(filename, dimensions, bufferR, bufferG, bufferB, buff
         tileNoY = int((z - tileNoX) / numColumns)
         tileOffset = tileNoX*(dimensions[0]+1)+tileNoY*width*(dimensions[1]+1)
 
-        for x in range(0,dimensions[0]):
+        #for y in range(0,dimensions[1]):
+        for y in yrange:
         
-            for y in range(0,dimensions[1]):
+            poffset = y*dimensions[0]+z*dimensions[0]*dimensions[1]
+        
+            #for x in range(0,dimensions[0]):
+            for x in xrange:
             
-                p = x+y*dimensions[0]+z*dimensions[0]*dimensions[1]
+                #p = x+y*dimensions[0]+z*dimensions[0]*dimensions[1]
+                p = poffset + x
 
                 # If R, G, or B are 'none' then 0.0 is assumed
                 valR = 0
@@ -208,7 +276,7 @@ def build_exr_from_buffers(filename, dimensions, bufferR, bufferG, bufferB, buff
                 else:
                     valA = 1.0
                 
-               # pixels[(y*width)+x+z*(dimensions[0]+1)] = [valR,valG,valB,valA]
+                #pixels[(y*width)+x+z*(dimensions[0]+1)] = [valR,valG,valB,valA]
                 pixels[tileOffset + x + y*width] = [valR,valG,valB,valA]
 
     # 'flatten' the array - so [R1,G1,B1,A1], [R2,G2,B2,A2], [R3,G3,B3,A3],.... becomes R1,B1,G1,A1,R2,G2,B2,A2,R3,G3,B3,A3,....    
@@ -258,7 +326,7 @@ class Fluid2EXR_Operator(bpy.types.Operator):
     #bl_options = {}
 
     frameNo: bpy.props.IntProperty(description='Enter the frame to convert', name='Frame')
-    smokeCacheName: bpy.props.StringProperty(description="The selection object's smoke cache name", name="Cache Name")
+    #smokeCacheName: bpy.props.StringProperty(description="The selection object's smoke cache name", name="Cache Name")
     multiRow: bpy.props.BoolProperty(description='Create multi-row image instead of a single row of tiles', name="Multi-Row")
        
 
@@ -299,12 +367,12 @@ class Fluid2EXR_Operator(bpy.types.Operator):
         
         #TODO: Use the modifier type rather than name and also check that it's set to a *smoke domain*
         
-        smokeCache = selected_object.modifiers['Fluid'].domain_settings.point_cache
+        #smokeCache = selected_object.modifiers['Fluid'].domain_settings.point_cache
         
-        self.smokeCacheName = smokeCache.name
-        print("Smoke cache name = "+str(self.smokeCacheName))
+        #self.smokeCacheName = smokeCache.name
+        #print("Smoke cache name = "+str(self.smokeCacheName))
         
-        self.smokeCache = smokeCache
+        #self.smokeCache = smokeCache
         #self.domainSettings = smokeCache = selected_object.modifiers['Smoke'].domain_settings
         self.domainSettings = selected_object.modifiers['Fluid'].domain_settings
         self.domainObject = selected_object
@@ -313,19 +381,20 @@ class Fluid2EXR_Operator(bpy.types.Operator):
 
     def execute(self, context):
   
-        smokeCacheName = str(self.smokeCacheName)
-        
-        if len(smokeCacheName) == 0:
-            self.report({'ERROR'}, "Smoke cache must be baked to a *named* cache.")
-            return {"CANCELLED"}
-            #TODO: Pickup the "random" generated name if we don't find an explicit name
+        #smokeCacheName = str(self.smokeCacheName)
+        #
+        #if len(smokeCacheName) == 0:
+        #    self.report({'ERROR'}, "Smoke cache must be baked to a *named* cache.")
+        #    return {"CANCELLED"}
+        #    #TODO: Pickup the "random" generated name if we don't find an explicit name
             
-        cachefile = bpy.path.abspath("//blendcache_%s/%s_%06i_00.bphys" % (bpy.path.basename(bpy.context.blend_data.filepath[:-6]),smokeCacheName,self.frameNo))
+        #cachefile = bpy.path.abspath("//blendcache_%s/%s_%06i_00.bphys" % (bpy.path.basename(bpy.context.blend_data.filepath[:-6]),smokeCacheName,self.frameNo))
         
         #hiresMultiplier = self.domainSettings.amplify + 1
-        hiresMultiplier = 1
+        #hiresMultiplier = 1
         
-        convert_volume_to_exr(self.domainObject, "%s_%06i", self.frameNo, self.multiRow, hiresMultiplier=hiresMultiplier)
+        #convert_volume_to_exr(self.domainObject, "%s_%06i", self.frameNo, self.multiRow, hiresMultiplier=hiresMultiplier)
+        convert_volume_to_exr(self.domainObject, "%s_%06i", self.frameNo, self.multiRow)
         return {'FINISHED'}
     
 
